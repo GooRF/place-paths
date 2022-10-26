da339a9ac8176865349a7b70afc9732924a8a16e
GooRF <jamesleung1201@outlook.com>
2022-10-26 17:03:57 +0800

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  <header>
+  </header>
+    <div class="main-content">
+      <h2 class="shadow-title">R/PLACE, BUT MATHS.</h2>
+    </div>
+</main>
+
+<style>
+.main-content{
+  display: flex;
+  flex-direction: row;
+  align-items:flex-start;
+}
+.shadow-title{
+  font-size: 3.2em;
+  border: .1em solid white;
+  padding: .2em 0.7em;
+  width: min-content;
+  text-align: center;
+  vertical-align: center;
+}
+</style>

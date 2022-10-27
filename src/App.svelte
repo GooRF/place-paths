2aa0be3010cb2e835bbe8788f169c1e09a790190
GooRF <jamesleung1201@outlook.com>
2022-10-27 17:10:31 +0800

    added tailwind to project

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,17 @@
 <main>
   <header>
   </header>
-    <div class="main-content">
-      <h2 class="shadow-title">R/PLACE, BUT MATHS.</h2>
-    </div>
+  <div class="flex gap-10">
+    <h2 class="shadow-cyan-600 shadow-2xl text-6xl w-min border-white border-solid border-8 py-5 px-6">
+      <span class="text-slate-400">R/PLACE</span>, 
+      <span>BUT</span> WITH
+      <span class="text-red-500">MATHS</span>.
+    </h2>
+    <section class="text-center w-80 flex flex-col gap-12">
+      <h3 class="text-5xl">How it Works</h3>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus numquam quis impedit dolores. Aperiam, sit, illum numquam ipsa eligendi omnis cum nisi hic quos repellat a est consequuntur! Rerum, facilis!
+      </div>
+    </section>  
+  </div>
 </main>
-
-<style>
-.main-content{
-  display: flex;
-  flex-direction: row;
-  align-items:flex-start;
-}
-.shadow-title{
-  font-size: 3.2em;
-  border: .1em solid white;
-  padding: .2em 0.7em;
-  width: min-content;
-  text-align: center;
-  vertical-align: center;
-}
-</style>

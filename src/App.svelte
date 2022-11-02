e1aa558cff58feb2eb41aa537c1b1e3656c72a62
GooRF <jamesleung1201@outlook.com>
2022-11-02 12:13:34 +0800

    added a neon-colored get started button

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+    import NeonButton from "./NeonButton.svelte";
 </script>
 
 <main>
-  <header>
-  </header>
-  <div class="flex gap-10">
-    <h2 class="shadow-cyan-600 shadow-2xl text-6xl w-min border-white border-solid border-8 py-5 px-6">
+  <div class="flex gap-10 h-screen w-screen items-center justify-center">
+    <h2 class="h-min shadow-cyan-600 shadow-2xl text-6xl w-min border-white border-solid border-8 py-5 px-6">
       <a href="https://www.reddit.com/r/place/" class="text-slate-300 motion-safe:animate-pulse">R/PLACE</a>, 
-      <span>BUT</span> WITH THE WORLD.
+      BUT WITH THE WORLD.
     </h2>
     <section class="text-center w-80 flex flex-col gap-12">
       <h3 class="text-5xl">How it Works</h3>
@@ -15,8 +14,8 @@
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus numquam quis impedit dolores. Aperiam, sit, illum numquam ipsa eligendi omnis cum nisi hic quos repellat a est consequuntur! Rerum, facilis!
       </div>
     </section>  
-    <nav>
-      
+    <nav class="flex flex-col items-center justify-center h-72 w-80">
+      <NeonButton />
     </nav>
   </div>
 </main>
---
+++ src/NeonButton.svelte
@@ -0,0 +1,58 @@
+<a href="/" class="neon-button">Get Started</a>
+<style>
+
+.neon-button {
+    font-size: 2rem;
+    color: var(--neon-color);
+
+    cursor: pointer;
+    display: inline-block;
+    text-decoration: none;
+    border: var(--neon-color) .125em solid;
+    padding: .25em 1em;
+    border-radius: .25em;
+
+    text-shadow: 
+        0 0 0.7em hsl(0 0% 100% / .9),
+        0 0 0.7em currentColor;
+
+    box-shadow: 0 0 2em 0 var(--neon-color);
+    position: relative;
+}
+.neon-button::before {
+    pointer-events: none;
+    content: "";
+    position: absolute;
+    background-color: var(--neon-color);
+    top: 160%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    transform: perspective(2em) rotateX(40deg);
+    filter: blur(0.8em);
+    opacity: 0.5;
+}
+.neon-button:hover,
+.neon-button:focus {
+    color: var(--background-color);
+    text-shadow: none;
+}
+.neon-button::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    box-shadow: 0 0 1em 0.5em var(--neon-color);
+    opacity: 0;
+    transition: opacity 100ms linear;
+    background-color: var(--neon-color);
+    z-index: -1;
+}
+.neon-button:hover::after, .neon-button:hover::before, 
+.neon-button:focus:after, .neon-button:focus::before {
+    opacity: 1;
+}
+</style>
